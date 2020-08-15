52e0f8263be80445c6eb1f95dd425e7473fac1f8
Elie Vangu <vangu.elie@gmail.com>
2020-08-15 20:30:22 +0200

    initiate alice and bob

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -1,9 +1,11 @@
 //Build Artifacts
-const CryptoZombies = artifacts.require("");
+const CryptoZombies = artifacts.require("CryptoZombies");
 
 //The contract() function
-contract("", () => {
-  it("", () => {
+contract("CryptoZombies", (accounts) => {
+  //initiate "alice" and "bob"
+  let [alice, bob] = accounts;
+  it("should be able to create a new zombie", () => {
 
   })
 })
