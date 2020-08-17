989a89c4175f3da90ccf7d8c97fad2f2dc3f7b97
Elie Vangu <vangu.elie@gmail.com>
2020-08-18 01:27:54 +0200

    instance of our contract

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -6,6 +6,7 @@ contract("CryptoZombies", (accounts) => {
   //initiate "alice" and "bob"
   let [alice, bob] = accounts;
   it("should be able to create a new zombie", async () => {
-
+    //Creation of an instance of our contract
+    const contractInstance = await CryptoZombies.new();
   })
 })
