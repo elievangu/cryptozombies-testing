cb381614d73ae32ac32eb62269d058252be22eff
Elie Vangu <vangu.elie@gmail.com>
2020-08-19 17:14:46 +0200

    insertion of beforEach function to prevent several initiation of the contract

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -5,10 +5,33 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 contract("CryptoZombies", (accounts) => {
   //initiate "alice" and "bob"
   let [alice, bob] = accounts;
+  
+  //variable limited in scope to the block in witch it's defined
+  let contractInstance;
+
+  //instead of writing contract.new() several times we use beforeEach()
+  beforeEach(async () => {
+    // let's put here the code that creates a new contract instance
+    contractInstance = await CryptoZombies.new();
+  });
+
+  /*selfdestruct is the only way for code at a certain address to be removed from the blockchain
+    function kill() public onlyOwner {
+      selfdestruct(owner());
+    }
+  */
+
+  //Truffle will make sure this function is called after a test gets executed
+  afterEach(async () => {
+    await contractInstance.kill();
+  });
+
   it("should be able to create a new zombie", async () => {
 
     //Creation of an instance of our contract
-    const contractInstance = await CryptoZombies.new();
+    //contractInstance = await CryptoZombies.new();
+
+    
 
     //Declaration of a const named result and set it equal to the result of contractInstance.createRandomZombie
     const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
@@ -18,5 +41,10 @@ contract("CryptoZombies", (accounts) => {
     
     //Check if result.logs[0].args.name equals to zombieNames[0] using assert.equal
     assert.equal(result.logs[0].args.name, zombieNames[0]);
+  })
+
+  //define the new it() function
+  it("should not allow two zombies", async () => {
+
   })
 })
