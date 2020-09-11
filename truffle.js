6eee48977fc39dfd04c5fcae7842f10e236e5f20
Elie Vangu <vangu.elie@gmail.com>
2020-09-11 12:40:51 +0200

    basechain object addition in truffle.js

--- truffle.js
+++ truffle.js
@@ -2,10 +2,20 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
 //Initialize LoomTruffleProvider
-const LoomTruffleProvider = require('loom-truffle-provider')
+const LoomTruffleProvider = require('loom-truffle-provider');
 
 // Set your own mnemonic here
 const mnemonic = "YOUR_MNEMONIC";
+
+const { readFileSync } = require('fs');
+const path = require('path');
+const { join } = require('path');
+
+//Function that reads the private key from a file and initializes a new LoomTruffleProvider
+function getLoomProviderWithPrivateKey (privateKeyPath, chainId, writeUrl, readUrl) {
+  const privateKey = readFileSync(privateKeyPath, 'utf-8');
+  return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
+}
 
 // Module exports to make this configuration available to Truffle itself
 module.exports = {
@@ -39,6 +49,20 @@ module.exports = {
         return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
         },
       network_id: '9545242630824'
+    },
+    
+    basechain: {
+      provider: function() {
+        const chainId = 'default';
+        const writeUrl = 'http://basechain.dappchains.com/rpc';
+        const readUrl = 'http://basechain.dappchains.com/query';
+        return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
+        const privateKeyPath = path.join(__dirname, 'mainnet_private_key');
+        const loomTruffleProvider = getLoomProviderWithPrivateKey(privateKeyPath, chainId, writeUrl, readUrl);
+        return loomTruffleProvider;
+        },
+      // Network id is * for Basechain
+      network_id: '*'
     }
   }
 }; 
